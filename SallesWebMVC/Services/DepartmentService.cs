af2f2c5df7ced68c73fded25c5c1c48332318d1d
Caio Maia <caiomaia3@gmail.com>
2021-12-17 21:48:01 -0300

    SellerFormViewModel and Department select component

---
+++ SallesWebMVC/Services/DepartmentService.cs
@@ -0,0 +1,23 @@
+﻿using SallesWebMVC.Data;
+using SallesWebMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace SallesWebMVC.Services
+{
+    public class DepartmentService
+    {
+        private readonly SallesWebMVCContext _context;
+
+        public DepartmentService(SallesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> ListAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
